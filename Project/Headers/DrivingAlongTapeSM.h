e71d4cf4f81e9c2c88225f7eb00ba418ba63579c
awarmoth <awarmoth@stanford.edu>
2017-02-27 06:15:12 -0800

    line following

--- Project/Headers/DrivingAlongTapeSM.h
+++ Project/Headers/DrivingAlongTapeSM.h
@@ -11,5 +11,6 @@ typedef enum { Waiting, Driving2Station, Driving2Reload} DrivingState_t ;
 
 ES_Event RunDrivingAlongTapeSM( ES_Event CurrentEvent );
 void StartDrivingAlongTapeSM ( ES_Event CurrentEvent );
+void SetController(uint8_t control);
 
 #endif /*DRIVING_ALONG_TAPE_SM_H */
