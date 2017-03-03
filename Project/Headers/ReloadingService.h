7ce46442895f769e1f2c184c1b7d2039df9dac25
awarmoth <awarmoth@stanford.edu>
2017-03-03 00:38:33 -0800

    Coded Reloading Service - need to implement EnableIREmitter function

---
+++ Project/Headers/ReloadingService.h
@@ -0,0 +1,16 @@
+//ReloadState_t: WaitingReload, Emitting_High, Emitting_Low, Wait4Delivery
+
+#ifndef RELOADING_SERVICE_H
+#define RELOADING_SERVICE_H
+
+#include "ES_Configure.h" /* gets us event definitions */
+#include "ES_Types.h"     /* gets bool type for returns */
+
+bool InitReloadingService(uint8_t Priority);
+bool PostReloadingService(ES_Event ThisEvent);
+ES_Event RunReloadingService(ES_Event ThisEvent);
+
+typedef enum {WaitingReload, Emitting_High, Emitting_Low, Wait4Delivery} FiringState_t ;
+
+
+#endif /*FIRING_SERVICE_H*/
