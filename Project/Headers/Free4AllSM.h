792087a4b78e21dbcf84e20fc3b41098f22d62cd
awarmoth <awarmoth@stanford.edu>
2017-03-03 00:55:26 -0800

    Started Free4All code

---
+++ Project/Headers/Free4AllSM.h
@@ -0,0 +1,25 @@
+FFAState_t: Waiting, Driving2Reload, AlignFFA, RapidFiring, Firing, Reloading
+
+#ifndef ConstructingSM_H
+#define ConstructingSM_H
+
+#include "ES_Configure.h" /* gets us event definitions */
+#include "ES_Types.h"     /* gets bool type for returns */
+
+// State definitions for use with the query function
+typedef enum { GettingTargetStation, DrivingAlongTape, CheckIn, Shooting, Reloading } ConstructingState_t ;
+
+// Public Function Prototypes
+
+ES_Event RunConstructingSM( ES_Event CurrentEvent );
+void StartConstructingSM ( ES_Event CurrentEvent );
+static ES_Event DuringGettingTargetStation( ES_Event Event);
+static ES_Event DuringDrivingAlongTape( ES_Event Event);
+static ES_Event DuringCheckIn( ES_Event Event);
+static ES_Event DuringShooting( ES_Event Event);
+static ES_Event DuringShooting( ES_Event Event);
+static ES_Event DuringReloading(ES_Event ThisEvent);
+void UpdateStatus( void );
+uint32_t getPeriod( void );
+uint8_t getBallCount(void);
+void SetBallCount(uint8_t count);
