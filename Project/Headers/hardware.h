1cddbfb748bf2f4e14dfe4396793bbf29ab547eb
iandevlaming <iandevlaming@gmail.com>
2017-03-07 13:03:17 -0800

    erp
    uh

--- Project/Headers/hardware.h
+++ Project/Headers/hardware.h
@@ -5,3 +5,4 @@ void InitializePins(void);
 void SetMotorController(uint8_t control);
 bool getISRFlag(void);
 void Set_Launcher_Command(uint16_t Requested_Command);
+void SetLED(uint8_t Mode, uint8_t LED);
